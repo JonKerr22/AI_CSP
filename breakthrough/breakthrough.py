8d0566de34a6c28f3cce12ad0d4a19a85d233b2b
JonKerr22 <jrkerr22@gmail.com>
2017-10-23 16:49:14 -0500

    took notes

--- breakthrough/breakthrough.py
+++ breakthrough/breakthrough.py
@@ -1,3 +1,9 @@
+#offensive hueristic 2: try and find moves that max the number of peices you attack
+	#defense 1 just defends itself doesn't attack your peices, so offense 2 beats it
+
+#defensive 2: try to bait offensive 1 to attack for no gain, in order to open up
+	#more paths to oppoenent homerow, not yet sure exactly how to choose where to bait opponent
+
 def Game:
 	def __init__(self):
 		
--- free_flow/free_flow.py
+++ free_flow/free_flow.py
@@ -7,6 +7,22 @@ class Game:
 
 
 	def dumbSolve(self):
-		#uninformed search, probably just modify our bfs
+		#attempt move
+		#check constraints
+		#if move valid, do move
+		#see if solved for 1 color
+			#see if solved for whole board
+			#check if solution prevents other solutions from happening
+				#if solution is too constraining, throw out try again
+
+		#uninformed search, probably just modify part of mp1
 	
 	def smartSolve(self):
+
+
+	def generateAllPaths(self):
+
+	def isPathLegal(self):
+
+	def findSolution(self):
+		#search all paths that all the colors can have, find the ones that don't overlap
